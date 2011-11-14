2328576bd9d77c033608650a3afae9ebdfc77ea9
itchyny <neknekneknek.t@gmail.com>
2011-11-15 01:31:16 +0900

    add footer.js, header.js, version.txt

---
+++ js/header.js
@@ -0,0 +1,12 @@
+/* * * * * * * * * * * * * * * * * * * * * * * * * * * * * * *\
+ *
+ *    Chrome Player @VERSION
+ *
+ *    author      : itchyny
+ *    last update : @DATE
+ *    source code : https://github.com/itchyny/ChromePlayer
+ *
+\* * * * * * * * * * * * * * * * * * * * * * * * * * * * * * */
+
+(function () {
+
