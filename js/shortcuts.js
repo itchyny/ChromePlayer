4abe34463a9cc93b3eadcd11d83f71810b771587
itchyny <neknekneknek.t@gmail.com>
2011-11-16 22:25:39 +0900

    shortcuts.js move to js/

---
+++ js/shortcuts.js
@@ -0,0 +1,41 @@
+//
+// This is the content script for Chrome Player.
+// On keydown event, check if its keyCode exists in the key set,
+// and if true, pass its meta keys to the main script.
+// key set is as follows:
+//   32: space
+//   37: left
+//   38: up
+//   39: right
+//   40: down
+//   119: f8
+//   120: f9
+//   121: f10
+//   173: volume mute
+//   174: volume down
+//   175: volume up
+//   176: media_next_track
+//   177: media_prev_track
+//   178: media_stop
+//   179: media_play_pause
+window.addEventListener(
+  'keydown',
+  (function(){
+    var keys = [ 32, 37, 38, 39, 40, 119, 120, 121, 173, 174, 175, 176, 177, 178, 179 ];
+    return function(e){
+      if(keys.indexOf(e.keyCode) >= 0){
+        try{
+          chrome.extension.sendRequest(
+            {
+              keyCode: e.keyCode,
+              shiftKey: e.shiftKey,
+              ctrlKey: e.ctrlKey,
+              altKey: e.altKey,
+              global: true
+            }
+          );
+        } catch(e){};
+      }
+    }
+  })()
+);
